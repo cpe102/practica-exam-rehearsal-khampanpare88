3d3eb93ef75cf8abfa951cac31956973dc31357a
lab314-48@labeng.local <lab314-48@labeng.local>
2020-01-10 17:17:25 +0700

    rehearsal_4

--- rehearsal_4.cpp
+++ rehearsal_4.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
-
-//Write compress() here.
-
+using namespace std;
+string compress(string x){
+    string s="";
+    int L= x.size();
+    int i=0;
+    while(i<L){
+        s=s+x[i];
+        i=i+3;
+    }
+    return s;
+} 
 int main()
 {
-    string a = conpress("ABCDEFGHIJKLMN");
-    string b = conpress("123456");
-    string c = conpress("HelloWorld");
-    string d = conpress("BNK48");
-    string e = conpress("COMPROG261102");
-    string f = conpress("A");
-    string g = conpress("AB");
-    string h = conpress("ABC");
-    string i = conpress("ABCD");
+    string a = compress("CPECMU");
+    string b = compress("X");
+    string c = compress("HelloWorld");
+    string d = compress("BNK48");
+    string e = compress("COMPROG261102");
+    string f = compress("A");
+    string g = compress("AB");
+    string h = compress("ABC");
+    string i = compress("ABCD");
     
     cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n" << h << "\n" << i << "\n";
 
