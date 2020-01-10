6915f2a845bd763d3dc3ce8a2e10d493800d0518
lab314-48@labeng.local <lab314-48@labeng.local>
2020-01-10 17:17:03 +0700

    rehearsal_3

--- rehearsal_3.cpp
+++ rehearsal_3.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
-
-//Write sumSqrt() here.
-
-
+#include<cmath>
+double sumSqrt(double N){
+    double sum=0;
+    int i=1;
+    if(N>0){
+        while(i<=N){
+            sum=sum+(1/(pow(i,0.5)));
+            i++;
+        }
+        return sum; 
+    }
+    else{ return 0;}
+}
 int main()
 {
-    double a = sumSqrt(20);
-    double b = sumSqrt(10);
+    double a = sumSqrt(-5);
+    double b = sumSqrt(3);
     double c = sumSqrt(5);
     double d = sumSqrt(2);
     double e = sumSqrt(1);
     double f = sumSqrt(0);
     double g = sumSqrt(-1);
     
-    cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n";
+    std::cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n";
 
 }
