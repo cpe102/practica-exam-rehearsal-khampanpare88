8ff075dc91e543eb9aca49e5d4287d2687b507f6
lab314-48@labeng.local <lab314-48@labeng.local>
2020-01-10 17:16:12 +0700

    rehearsal_1

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{   string x;
+    double y;
+    cout<<"What is your name: ";
+    cin>>x;
+    cout<<"What is your GPA: ";
+    cin>>y;
+    if(y>=3.5){
+        cout<<x<<" InW Jrim Jrim!!!";
+    }
+    else
+    {
+        cout<<"Try harder, "<<x<<"!!!";
+    }
+    
+    return 0;
+}
